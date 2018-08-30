cd1d1682dc488280b2a66a9911e4ceeaf2789df9
Andrew <aturnage@hotmail.com>
2018-08-30 07:43:45 -0500

    MTB Band full site updates

---
+++ scripts/mtb.js
@@ -0,0 +1,12 @@
+// $( document ).ready( () => {
+//     $( ".events" ).on("mouseenter", ( event ) => {
+//         console.log( event );
+//         $( "#sheldonInfo" ).slideDown( "slow" );
+//     } );
+// } );
+$(".video").click(function () {
+        alert("Single Click");
+    });
+$(".eventsPoster").click(function () {
+        $("#sheldonInfo").slideDown("slow");
+    });
